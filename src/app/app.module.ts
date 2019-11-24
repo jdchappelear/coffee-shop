f299d368c9cae38723deb6a9b0e9e216f77c00fc
John Chappelear <john.chappelear@gmail.com>
2019-11-24 13:46:06 -0500

    component setup - ready to start Firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './orders/orders.component';
 import { OrderListComponent } from './order-list/order-list.component';
+import { OrdersService } from "./shared/orders.service";
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { OrderListComponent } from './order-list/order-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [OrdersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from "../shared/orders.service";
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ordersService:OrdersService) { }
 
   ngOnInit() {
   }
 
+  coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", 
+      "Machiato", "Mocha", "Hot Chocolate", "Tea"];
+
+    coffeeOrder = [];
+    
+    addCoffee = coffee => this.coffeeOrder.push(coffee);
+
+    removeCoffee = coffee => {
+      let index = this.coffeeOrder.indexOf(coffee);
+      if (index > -1) this.coffeeOrder.splice(index, 1);
+    };
+
+    onSubmit() {}
 }
